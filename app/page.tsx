508d24d554347c6846118fdcc63d92fb4232b58f
karen-vollan <karen.vollan@hotmail.com>
2024-08-13 17:01:31 +0200

    fix banners url

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Dashboard() {
           </h2>
         </a>
         <a
-          href="http://localhost:3000/banners"
+          href="./banners"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
