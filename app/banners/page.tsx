f383550bb546e1587753b356e31f358c003d9b12
karen-vollan <karen.vollan@hotmail.com>
2024-08-18 19:19:11 +0200

    add ui to banners-page

--- app/banners/page.tsx
+++ app/banners/page.tsx
@@ -76,9 +76,15 @@ export default function Banners() {
         <tbody>
           {banners.map((banner, index) => (
             <tr key={index} className="bg-white dark:bg-gray-800">
-            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              {banner.name}
+            {banner.placed ? (
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray-500">
+              <span style={{ textDecoration: 'line-through' }}>{banner.name}</span>
             </th>
+             ) : (
+             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray-100">
+             {banner.name}
+           </th>)
+           }
             <td className="px-6 py-4">
               {banner.id}
             </td>
