91d9053227294a1ee60647ffc3124df5417dc559
karen-vollan <karen.vollan@hotmail.com>
2024-08-18 14:47:05 +0200

    can change banners placed or not

--- app/banners/page.tsx
+++ app/banners/page.tsx
@@ -6,7 +6,7 @@
 // imports
 import { useState, useEffect } from 'react';
 import { initializeApp } from "firebase/app"
-import { getFirestore, doc, onSnapshot} from 'firebase/firestore';
+import { getFirestore, doc, onSnapshot, getDoc, updateDoc} from 'firebase/firestore';
 
 // create connection with firestore
 const firebaseConfig = {
@@ -26,6 +26,22 @@ interface Banner {
   placed: boolean;
 }
 
+// update firestore with the new banners
+async function updateBanners(index:number, bannerPlaced:Boolean) {
+  // get banners from the list allBanners in 
+  // collection "banners" - document "all" - field "allBanners"
+  const docRef = doc(db, "banners", "all")
+  const docSnap = await getDoc(docRef)
+  const allBanners = docSnap.data()?.allBanners ?? []
+
+  // change banner
+  allBanners[index]["placed"] = !bannerPlaced
+
+  // update document
+  await updateDoc(docRef, {allBanners})
+  
+}
+
 export default function Banners() {
   const [banners, setBanners] = useState<Banner[]>([])
   useEffect(() => {
@@ -35,10 +51,13 @@ export default function Banners() {
     });
     return () => unsub();
   }, []);
+
+  const handleClick = (index:number, bannerPlaced:Boolean) => {
+    updateBanners(index, bannerPlaced)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">  
-    <div className="relative overflow-x-auto">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
@@ -54,7 +73,7 @@ export default function Banners() {
         </thead>
         <tbody>
           {banners.map((banner, index) => (
-            <tr className="bg-white dark:bg-gray-800">
+            <tr key={index} className="bg-white dark:bg-gray-800">
             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
               {banner.name}
             </th>
@@ -62,13 +81,11 @@ export default function Banners() {
               {banner.id}
             </td>
             <td className="px-6 py-4">
-              {banner.placed ? "Yes" : "No"}
+              <button onClick={() => handleClick(index, banner.placed)}>{banner.placed ? "Yes" : "No"}</button>
             </td>
             </tr>
-          ))} 
+          ))}
         </tbody>
       </table>
-    </div>
-  </main>
   );
 }
